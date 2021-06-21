e472258a45d41b58e808ee0c6c875571ce693d57
qingyao yang <yangqy@inhand.com.cn>
2021-06-21 23:04:26 +0800

    listNode

---
+++ array/problem260/ReverseLinkedList_0.py
@@ -0,0 +1,11 @@
+def reverseList(head):
+    index = 0
+    while index < len(head)/2:
+        tail = (len(head)) - 1 - index
+        head[index], head[tail] = head[tail], head[index]
+        index = index + 1
+    return head
+
+
+if __name__ == '__main__':
+    print(reverseList(head = [1,2,3,4,5]))
