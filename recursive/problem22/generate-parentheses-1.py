95fc4dfffb2fc057d317b76d55715743821cb898
yangqingyao <yangqy@inhand.com.cn>
2021-10-08 11:15:58 +0800

    leetcode problem 222

---
+++ recursive/problem22/generate-parentheses-1.py
@@ -0,0 +1,28 @@
+def generateParenthesis(n: int):
+    if n <= 0:
+        return []
+    res = []
+
+    def dfs(paths, left, right):
+        if left > n or right > left:
+            return
+        if len(paths) == n * 2:  # 因为括号都是成对出现的
+            res.append(paths)
+            return
+
+        dfs(paths + '(', left+1, right)
+        dfs(paths + ')', left, right+1)
+
+    dfs('', 0,0)
+
+    return res
+
+
+class Solution:
+    pass
+
+
+if __name__ == '__main__':
+    s = Solution()
+    a = generateParenthesis(n=3)
+    print(a)
