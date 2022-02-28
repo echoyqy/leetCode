fa9df1081acb35787dbe04370f2a7c1639810b1e
yangqy <yangqy@inhand.com.cn>
2022-02-28 18:08:52 +0800

    add new train

--- tree/problem208/implement-trie-prefix-tree-1.py
+++ tree/problem208/implement-trie-prefix-tree-1.py
@@ -6,29 +6,21 @@ class Trie:
     def searchPrefix(self, prefix: str) -> "Trie":
         node = self
         for ch in prefix:
-            ch = ord(ch) - ord("a")
+            ch = ord(ch) - ord('a')
             if not node.children[ch]:
                 return None
             node = node.children[ch]
         return node
+
     def insert(self, word: str) -> None:
-        nodes = self
+        node = self
         for ch in word:
-            ch = ord(ch) - ord("a")
-            if not nodes.children[ch]:
-                nodes.children[ch] = Trie()
-            nodes = nodes.children[ch]
-        nodes.isEnd = True
+            ch = ord(ch) - ord('a')
+            if not node.children[ch]:
+                node.children[ch] = Trie()
+            node = node.children[ch]
+        node.isEnd = True
 
     def search(self, word: str) -> bool:
         node = self.searchPrefix(word)
         return node is not None and node.isEnd
-
-    def startsWith(self, prefix: str) -> bool:
-        return self.searchPrefix(prefix) is not None
-
-
-if __name__ == '__main__':
-    trie = Trie()
-    trie.insert("apple")
-    trie.search("apple")
